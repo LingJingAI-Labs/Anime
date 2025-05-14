26fb3c90abafd21d6532585956d616762ec53067
Snychng <snychng@gmail.com>
2025-05-15 00:06:45 +0800

    chore: 启用所有服务器IP以增加并发工作线程数

--- src/comfyui_redrawer_0514-wuji.py
+++ src/comfyui_redrawer_0514-wuji.py
@@ -33,17 +33,17 @@ except ImportError:
 # --------------- 配置参数 ---------------
 SERVER_IPS = [
     "http://comfyui-demo.lingjingai.cn",
-    # "http://comfyui-demo2.lingjingai.cn",
-    # "http://comfyui-demo3.lingjingai.cn",
-    # "http://comfyui-demo4.lingjingai.cn",
-    # "http://comfyui-demo5.lingjingai.cn",
-    # "http://comfyui-demo6.lingjingai.cn",
-    # "http://comfyui-demo7.lingjingai.cn",
-    # "http://comfyui-demo8.lingjingai.cn",
-    # "http://comfyui-demo9.lingjingai.cn",
-    # "http://comfyui-demo10.lingjingai.cn",
-    # "http://comfyui-demo11.lingjingai.cn",
-    # "http://comfyui-demo12.lingjingai.cn",
+    "http://comfyui-demo2.lingjingai.cn",
+    "http://comfyui-demo3.lingjingai.cn",
+    "http://comfyui-demo4.lingjingai.cn",
+    "http://comfyui-demo5.lingjingai.cn",
+    "http://comfyui-demo6.lingjingai.cn",
+    "http://comfyui-demo7.lingjingai.cn",
+    "http://comfyui-demo8.lingjingai.cn",
+    "http://comfyui-demo9.lingjingai.cn",
+    "http://comfyui-demo10.lingjingai.cn",
+    "http://comfyui-demo11.lingjingai.cn",
+    "http://comfyui-demo12.lingjingai.cn",
     # 根据需要添加其他服务器IP
 ]
 NUM_WORKERS = len(SERVER_IPS) # 并发工作线程数 = 服务器数量
